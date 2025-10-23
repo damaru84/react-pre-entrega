8da6ae550ff48c98e0cba9063fd82f2e13fe5b9e
“damaru84” <info.danielrubio@gmail.com>
2025-10-23 19:50:33 -0300

    recuperado falla boton

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,15 +2,25 @@
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../context/CartContext/useCartContext";
 import "./Nav.css";
-
+import logo from "../../assets/logo.png";
 
 export const Nav = () => {
   const { getTotalItems } = useCartContext();
 
   
   return (
-    <nav>
-      <ul>
+<nav className="navbar">
+      {/* 🔹 Logo a la izquierda */}
+      <Link to="/" className="logo-link">
+        <img src={logo} alt="Logo" className="logo" />
+      </Link>
+
+      {/* 🔹 Menú de navegación */}
+      <ul className="menu">
+
+
+
+   
         <li>
           <Link to="/">Home</Link>
         </li>
