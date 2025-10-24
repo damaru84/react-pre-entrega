bfa70a35efaf48218f69ba1a0c2b6c32524fdcef
“damaru84” <info.danielrubio@gmail.com>
2025-10-23 22:04:45 -0300

    cambios para deploy

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,21 +1,3 @@
-/*import { Link } from "react-router-dom";
-import { Item } from "../Item/Item";
-
-export const ItemList = ({ lista }) => {
-  return (
-    <>
-      {lista.length ? (
-        lista.map((producto) => (
-          <Link to={`/detail/${producto.id}`} key={producto.id}>
-            <Item {...producto} />
-          </Link>
-        ))
-      ) : (
-        <p>No hay productos</p>
-      )}
-    </>
-  );
-};*/
 import { Link } from "react-router-dom";
 import { Item } from "../Item/Item";
 import "./ItemList.css"; // 👈 Importá el CSS
